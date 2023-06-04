106b606a1f6b03d58afd8b9418354f0ced3a64a5
thisismihai <mihai.badea@pantopia.tech>
2023-06-04 20:22:33 +0300

    update update-user

--- gapi/rpc_update_user.go
+++ gapi/rpc_update_user.go
@@ -16,42 +16,43 @@ import (
 )
 
 func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
-
 	authPayload, err := server.authorizeUser(ctx)
 	if err != nil {
 		return nil, unauthenticatedError(err)
 	}
 
 	violations := validateUpdateUserRequest(req)
-
 	if violations != nil {
 		return nil, invalidArgumentError(violations)
 	}
 
 	if authPayload.Username != req.GetUsername() {
-		return nil, status.Errorf(codes.PermissionDenied, "cannot update other users info")
+		return nil, status.Errorf(codes.PermissionDenied, "cannot update other user's info")
 	}
 
 	arg := db.UpdateUserParams{
 		Username: req.GetUsername(),
 		FullName: sql.NullString{
 			String: req.GetFullName(),
-			Valid:  req.GetFullName != nil,
+			Valid:  req.FullName != nil,
 		},
 		Email: sql.NullString{
 			String: req.GetEmail(),
-			Valid:  req.GetEmail != nil,
+			Valid:  req.Email != nil,
 		},
 	}
+
 	if req.Password != nil {
 		hashedPassword, err := util.HashPassword(req.GetPassword())
 		if err != nil {
-			return nil, status.Errorf(codes.Internal, "Failed to Hash Password: %s", err)
+			return nil, status.Errorf(codes.Internal, "failed to hash password: %s", err)
 		}
+
 		arg.HashedPassword = sql.NullString{
 			String: hashedPassword,
 			Valid:  true,
 		}
+
 		arg.PasswordChangedAt = sql.NullTime{
 			Time:  time.Now(),
 			Valid: true,
@@ -60,12 +61,10 @@ func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest)
 
 	user, err := server.store.UpdateUser(ctx, arg)
 	if err != nil {
-
 		if err == sql.ErrNoRows {
-			return nil, status.Errorf(codes.Internal, "User Not Found")
+			return nil, status.Errorf(codes.NotFound, "user not found")
 		}
-
-		return nil, status.Errorf(codes.Internal, "Failed to Update User: %s", err)
+		return nil, status.Errorf(codes.Internal, "failed to update user: %s", err)
 	}
 
 	rsp := &pb.UpdateUserResponse{
@@ -78,6 +77,7 @@ func validateUpdateUserRequest(req *pb.UpdateUserRequest) (violations []*errdeta
 	if err := val.ValidateUsername(req.GetUsername()); err != nil {
 		violations = append(violations, fieldViolation("username", err))
 	}
+
 	if req.Password != nil {
 		if err := val.ValidatePassword(req.GetPassword()); err != nil {
 			violations = append(violations, fieldViolation("password", err))
@@ -89,10 +89,12 @@ func validateUpdateUserRequest(req *pb.UpdateUserRequest) (violations []*errdeta
 			violations = append(violations, fieldViolation("full_name", err))
 		}
 	}
+
 	if req.Email != nil {
 		if err := val.ValidateEmail(req.GetEmail()); err != nil {
 			violations = append(violations, fieldViolation("email", err))
 		}
 	}
+
 	return violations
 }
